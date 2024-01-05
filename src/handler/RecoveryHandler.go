b13d5c79cecf8acd7f32e747aeba58dec3c9fcd9
GAXING <gao.xing@thoughtworks.com>
2024-01-05 17:02:53 +0800

    refactor revovery handler

--- src/handler/RecoveryHandler.go
+++ src/handler/RecoveryHandler.go
@@ -6,16 +6,19 @@ import (
 	"runtime/debug"
 )
 
-func RecoveryHandler(context *gin.Context) {
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println("Panic recovered:", err)
-			log.Println("Stack trace:", string(debug.Stack()))
-			context.JSON(500, gin.H{
-				"code": 500,
-				"msg":  "Internal Server Error",
-			})
-		}
-	}()
-	context.Next()
+func RecoveryHandler() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println("Panic recovered:", err)
+				log.Println("Stack trace:", string(debug.Stack()))
+				context.JSON(500, gin.H{
+					"code": 500,
+					"msg":  "Internal Server Error",
+				})
+				context.Abort()
+			}
+		}()
+		context.Next()
+	}
 }
